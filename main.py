5b4692187afa2c6832ace7bdfa4e45f876edc86d
ChipsMaster2 <prakashankkit@gmail.com>
2022-05-18 09:35:55 -0700

    fixed example, added /h

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ with open("private.json","r") as file:
 
 client = discord.Client()
 
-dictTI = {"/j":"joking","/hj":"half-joking", "/s":"sarcastic", "/gen":"genuine", "/g":"genuine","/srs":"serious","/nsrs":"non-serious","/pos":"positive connotation","/pc":"positive connotation","/neu":"neutral connotation","/neg":"negative connotation","/nc":"negative connotation","/p":"platonic","/r":"romantic","/c":"copypasta","/l":"lyrics","/ly":"lyrics",'/lh':"light-hearted","/nm":"not mad","/lu":"a little upset","/nbh":"for when you're vagueposting or venting, but it's directed at **nobody here**","/sx":"sexual intent","/x":"sexual intent","/nsx":"non-sexual intent","/nx":"non-sexual intent","/rh":"rhetorical question","/rt":"rhetorical question","/t":"teasing","/ij":"inside joke","/m":"metaphorically","/li":"literally","/hyp":"hpyerbole","/f":"fake","/th":"threat","/cb":"clickbait"}
+dictTI = {"/j":"joking","/hj":"half-joking", "/s":"sarcastic", "/gen":"genuine", "/g":"genuine","/srs":"serious","/nsrs":"non-serious","/pos":"positive connotation","/pc":"positive connotation","/neu":"neutral connotation","/neg":"negative connotation","/nc":"negative connotation","/p":"platonic","/r":"romantic","/c":"copypasta","/l":"lyrics","/ly":"lyrics",'/lh':"light-hearted","/nm":"not mad","/lu":"a little upset","/nbh":"for when you're vagueposting or venting, but it's directed at **nobody here**","/sx":"sexual intent","/x":"sexual intent","/nsx":"non-sexual intent","/nx":"non-sexual intent","/rh":"rhetorical question","/rt":"rhetorical question","/t":"teasing","/ij":"inside joke","/m":"metaphorically","/li":"literally","/hyp":"hpyerbole","/f":"fake","/th":"threat","/cb":"clickbait","/h":"humor"}
 
 @client.event
 async def on_ready():
@@ -24,7 +24,7 @@ async def on_message(message):
             print(message.content[len(PREFIX):].lower().strip() in [""])
             embed=discord.Embed(title="Tone Bot Help", description="Dictionary Bot For Tone Indicators", color=0x9141ac)
             embed.add_field(name="Prefix: ", value="!tb", inline=True)
-            embed.add_field(name="Usage (example):", value="!tb \srs", inline=True)
+            embed.add_field(name="Usage (example):", value="!tb /srs", inline=True)
             embed.set_footer(text="Returns not nound if Tone Indicator is not present or if there is no slash in front")
             await message.reply(embed=embed,mention_author=False)
             # help embed
