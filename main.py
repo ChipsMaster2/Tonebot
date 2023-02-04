40f5f9c5acc2d0894b80ea48d623d09529f3086a
Soham Nandy <soham.nandy@outlook.com>
2023-02-04 15:12:35 +0530

    fixed asking for all intent

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ except:
     with open("config.json","w") as file:
         file.write(json_object)
 
-intents = discord.Intents.all()
+intents = discord.Intents(guild_messages=True,message_content=True,messages=True)
+
 client = discord.Client(intents=intents)
 
 dictTI = {"/j":"joking","/hj":"half-joking", "/s":"sarcastic", "/gen":"genuine", "/g":"genuine","/srs":"serious","/nsrs":"non-serious","/pos":"positive connotation","/pc":"positive connotation","/neu":"neutral connotation","/neg":"negative connotation","/nc":"negative connotation","/p":"platonic","/r":"romantic","/c":"copypasta","/l":"lyrics","/ly":"lyrics",'/lh':"light-hearted","/nm":"not mad","/lu":"a little upset","/nbh":"for when you're vagueposting or venting, but it's directed at **nobody here**","/sx":"sexual intent","/x":"sexual intent","/nsx":"non-sexual intent","/nx":"non-sexual intent","/rh":"rhetorical question","/rt":"rhetorical question","/t":"teasing","/ij":"inside joke","/m":"metaphorically","/li":"literally","/hyp":"hyperbole","/f":"fake","/th":"threat","/cb":"clickbait","/h":"humor"}
